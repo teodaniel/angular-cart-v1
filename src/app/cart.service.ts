0fd0a0a9be935fa80a4ee32301dcf6e67ba652ec
teodaniel <teodaniel14@gmail.com>
2022-03-29 17:47:13 +0800

    2 files modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -27,6 +27,6 @@ export class CartService {
   }
 
   getShippingPrices() {
-    return this.http.get< { type: string, price: number } >('/assets/shipping.json');
+    return this.http.get< {type: string, price: number } []>('/assets/shipping.json');
   }
 }
