297ebe58ac4e851ac061d8dc4b317a03db9b1fce
teodaniel <teodaniel14@gmail.com>
2022-03-29 10:07:50 +0800

    2 files modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+//  import router module
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 
@@ -7,21 +8,27 @@ import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     ReactiveFormsModule,
+    // add to imports array
+    // configure the router module
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       // { path: 'prodlist', component: ProductListComponent },
+      // configure the path with a parameter
+      { path: 'products/:productId', component:ProductDetailsComponent}
     ])
   ],
   declarations: [
     AppComponent,
     TopBarComponent,
     ProductListComponent,
-    ProductAlertsComponent
+    ProductAlertsComponent,
+    ProductDetailsComponent
   ],
   bootstrap: [
     AppComponent
